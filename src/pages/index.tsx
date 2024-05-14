5bd310939db548f94cb724db1f2d6a89961905fd
ひろ <koki.hiro0723@gmail.com>
2024-05-15 02:00:13 +0900

    ✨ Loaderを追加した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { GetStaticProps, NextPage } from "next";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Loader } from "semantic-ui-react";
 
 interface SearchCatImage {
   id: string;
@@ -16,24 +18,32 @@ interface IndexPageProps {
 const fetchCatImage = async (): Promise<SearchCatImage> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const result = await res.json();
-  // console.log(result[0]);
   return result[0];
 };
 
 const Home: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
   const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = async () => {
+    setIsLoading(true);
     const catImage = await fetchCatImage();
     setCatImageUrl(catImage.url);
+    setIsLoading(false);
   };
 
   return (
     <>
       <div className={styles.main}>
         <h1>猫画像アプリ</h1>
-        <img className={styles.img} src={catImageUrl} alt="猫画像" />
-        <button onClick={handleClick}>今日の猫さん</button>
+        {isLoading ? (
+          <Loader active size="huge" inline="centered" />
+        ) : (
+          <div className={styles.display}>
+            <img className={styles.img} src={catImageUrl} alt="猫画像" />
+            <button onClick={handleClick}>今日の猫さん</button>
+          </div>
+        )}
       </div>
     </>
   );
